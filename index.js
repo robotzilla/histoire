1bec3c555d6ad75cd6767d4d580759ce3d51784f
Steve Fink <sphink@gmail.com>
2018-10-02 10:36:52 -0700

    Handle overall lookup status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // FIXME! This always appends at the end. If we're loading multiple chunks, we
 // want them inserted in order.
-function dataLoaded(xhr, start, end) {
+function addItems(rawText, start, end) {
     const list = document.getElementById("thelist");
 
     const addItem = line => {
@@ -10,12 +10,7 @@ function dataLoaded(xhr, start, end) {
         list.appendChild(item);
     };
 
-    if (xhr.status == 404) {
-        // Not found. That's fine; there might not be any entries for that time range.
-        return;
-    }
-
-    for (const line of xhr.responseText.split("\n")) {
+    for (const line of rawText.split("\n")) {
         if (line == "")
             continue;
         const [when, message] = line.split(" ", 1);
@@ -25,9 +20,33 @@ function dataLoaded(xhr, start, end) {
     }
 }
 
-function loadNotes(user, when, start, end) {
+function dataLoaded(xhr, start, end, info) {
+    info.sofar++;
+
+    // 404 is ok; there might not be any entries for that time range.
+    if (xhr.status != 404) {
+        info.found++;
+        addItems(xhr.responseText, start, end);
+    }
+
+    if (info.sofar < info.total)
+        return;
+
+    // All attempted data is loaded.
+
+    var header = document.getElementById("header");
+
+    if (info.found == 0) {
+        var header = document.getElementById("header");
+        header.textContent = "No updates found!";
+    } else {
+        header.textContent = "Updates for " + user;
+    }
+}
+
+function loadNotes(user, when, start, end, info) {
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', ev => dataLoaded(xhr, start, end));
+    xhr.addEventListener('load', ev => dataLoaded(xhr, start, end, info));
     xhr.open("GET", `users/${user}/${user}.${when}.txt`);
     xhr.send();
 }
@@ -45,12 +64,19 @@ function loadUserNotes(user, start, end) {
         start = end - 60 * 60 * 24 * 7;
 
     // Look back 1 week
+    let count = 0;
     for (let t = computeEra(start); t <= computeEra(end); t += eraSeconds)
-        loadNotes(user, t, start, end);
+        count++;
+
+    const info = {
+        'total': count,
+        'sofar': 0,
+        'found': 0
+    };
+    for (let t = computeEra(start); t <= computeEra(end); t += eraSeconds)
+        loadNotes(user, t, start, end, info);
 }
 
 var params = new URL(document.location).searchParams;
 var user = params.get("user");
-var header = document.getElementById("header");
-header.textContent = "Some updates for " + user;
-loadUserNotes(user, params.get("start"), params.get("end"));
+ploadUserNotes(user, params.get("start"), params.get("end"));
